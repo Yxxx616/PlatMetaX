a01d9599b768a04620ad6ebdce71d26c41fdbb72
Yxxx616 <yangxunudt@outlook.com>
2025-04-17 10:28:50 +0800

    repair Test function by command

--- Test.m
+++ Test.m
@@ -11,8 +11,13 @@ classdef Test < handle
     methods
         function obj = Test(moName, BO, envName, problemset)
             obj.moName = moName;
-            obj.boName = class(BO);
-            obj.BaseOptimizer = BO;
+            if isa(BO, 'handle')
+                obj.BaseOptimizer = BO;
+                obj.boName = class(BO);
+            else
+                obj.boName = BO;
+                obj.BaseOptimizer = feval(BO);
+            end
             if isa(problemset,'struct')
                 obj.TestingSetName = problemset.psName;
                 [~, obj.TestingSet] = splitProblemSet(problemset);
@@ -22,16 +27,14 @@ classdef Test < handle
             end
             obj.env = feval(envName, obj.TestingSet, obj.BaseOptimizer, 'test');
             fn = load(['AgentModel/', moName, '_finalAgent.mat']);
-            obj.MetaOptimizer = fn.agent;
+            obj.MetaOptimizer = fn.saved_agent;
         end
         
         function results = run(obj)
             set(0, 'DefaultFigureVisible', 'on');
             simOpts = rlSimulationOptions('NumSimulations',length(obj.TestingSet)); 
             testingInfo = sim(obj.env,obj.MetaOptimizer,simOpts);
-%             bestPops = obj.env.getBestPops();
             results.testingInfo = testingInfo;
-%             results.bestPops = bestPops;
         end
     end
 end
